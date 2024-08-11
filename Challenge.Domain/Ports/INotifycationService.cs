f80377e59f9cc6a35179c06e5f2ae384050b86d4
Gerson Sanchez <gerson.sanchez@ceiba.com.co>
2024-08-11 01:27:39 -0500

    Domain: desarrollo de puertos

--- Challenge.Domain/Ports/INotifycationService.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Challenge.Domain.Ports
-{
-	public interface INotifycationService : IDisposable
-	{
-		Task PublisMessageAtBroker(CancellationToken cancellationToken, string messageBody);
-		Task ReadMessageFromBroker(CancellationToken cancellationToken);
-	}
-}
--- Challenge.Domain/Ports/IReservationsRepository.cs
+++ Challenge.Domain/Ports/IReservationsRepository.cs
@@ -6,6 +6,6 @@ namespace Challenge.Domain.Ports
 {
 	public interface IReservationsRepository : ISave<Reservation>, IGet<Reservation>, IUpdate<Reservation>, IDelete<Reservation>, ISaveChange
 	{
-		Task<Reservation[]> GetMultipleByIdAsync(Guid[] ids, CancellationToken cancellationToken = default);
+		Task<Reservation[]> GetMultipleByIdAndIncludeAnyAsync(Guid[] ids, CancellationToken cancellationToken = default);
 	}
 }
