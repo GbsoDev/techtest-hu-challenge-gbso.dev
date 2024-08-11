909f95a9acea82159708db6fbf97d3723c78dfe1
Gerson Sanchez <gerson.sanchez@ceiba.com.co>
2024-08-10 23:40:45 -0500

    Workers: Conexión y configuración de servicios y base de datos

--- Challenge.Broker.NotificationWorker/Program.cs
+++ Challenge.Broker.NotificationWorker/Program.cs
@@ -1,7 +1,15 @@
 using Challenge.Broker.NotificationWorker;
+using Challenge.Broker.NotificationWorker.Helpers;
+using Challenge.Infrastructure.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+//Custom
+builder.Services.ConfigureOptions(builder.Configuration, WorkerAssemblyHelper.GetWorkerAssembly);
+builder.Services.AddLazyLoadingSupport();
+builder.Services.AddAdapters();
+//End Custom
 
 var host = builder.Build();
 host.Run();
--- Challenge.Outbox.PublisherWorker/Program.cs
+++ Challenge.Outbox.PublisherWorker/Program.cs
@@ -1,7 +1,23 @@
+using Challenge.Application.Helpers;
+using Challenge.Domain.Helpers;
+using Challenge.Infrastructure.Extensions;
 using Challenge.Outbox.PublisherWorker;
+using Challenge.Outbox.PublisherWorker.Helpers;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+//Custom
+builder.Services.ConfigureOptions(builder.Configuration, DomainAssemblyHelper.GetDomainAssembly);
+builder.Services.ConfigureOptions(builder.Configuration, WorkerAssemblyHelper.GetWorkerAssembly);
+builder.Services.AddLazyLoadingSupport();
+builder.Services.AddEfStorageProvider(builder.Configuration);
+builder.Services.AddDomainServices();
+builder.Services.AddAdapters();
+builder.Services.AddAutoMapper(ApplicationAssemblyHelper.GetApplicationAssembly);
+builder.Services.AddMediatR(ApplicationAssemblyHelper.GetApplicationAssembly);
+//End Custom
+
 
 var host = builder.Build();
 host.Run();
