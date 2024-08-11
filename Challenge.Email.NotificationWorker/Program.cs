a4d5a37c371f6255481c72c6b43353f19b4d22b0
Gerson Sanchez <gerson.sanchez@ceiba.com.co>
2024-08-10 23:35:31 -0500

    Email.NotificationWorker: desarrollo de worker de email

---
+++ Challenge.Email.NotificationWorker/Program.cs
@@ -0,0 +1,7 @@
+using Challenge.Email.NotificationWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
