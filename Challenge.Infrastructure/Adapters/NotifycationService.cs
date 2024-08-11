16fbab337c8a89054e0a11d2f81dac60b15d2eb2
Gerson Sanchez <gerson.sanchez@ceiba.com.co>
2024-08-10 22:16:03 -0500

    Infrastructre: Servicios externos

--- Challenge.Infrastructure/Adapters/NotifycationService.cs
+++ Challenge.Infrastructure/Adapters/NotifycationService.cs
@@ -1,23 +1,49 @@
+using Challenge.Domain.Entities.Reservations;
+using Challenge.Domain.Ports;
+using Challenge.Infrastructure.Options;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace Challenge.Infrastructure.Adapters
 {
-	public class NotifycationService : IDisposable
+	[Adapter]
+	public class NotifycationService : IBrokerService
 	{
+		private readonly ILogger<NotifycationService> _logger;
+		private readonly IOptions<RabbitMQOptions> _rabbitMQOptions;
+
 		private readonly IConnection _connection;
 		private readonly IModel _channel;
 
-		public NotifycationService()
+		public NotifycationService(ILogger<NotifycationService> logger, IOptions<RabbitMQOptions> rabbitMQOptions)
 		{
-			var factory = new ConnectionFactory() { HostName = "localhost" };
+			var factory = CreateConnectionFactory();
 			_connection = factory.CreateConnection();
 			_channel = _connection.CreateModel();
 
-			_channel.QueueDeclare(queue: "tu_cola", durable: false, exclusive: false, autoDelete: false, arguments: null);
+			_channel.QueueDeclare(queue: nameof(Reservation), durable: false, exclusive: false, autoDelete: false, arguments: null);
+			_logger = logger;
+			_rabbitMQOptions = rabbitMQOptions;
 		}
-		protected async Task ExecuteAsync(CancellationToken stoppingToken)
+
+		public async Task PublisMessageAtBroker(CancellationToken cancellationToken, string messageBody)
+		{
+			var body = Encoding.UTF8.GetBytes(messageBody);
+
+			if (!cancellationToken.IsCancellationRequested)
+			{
+				_channel.BasicPublish(exchange: "",
+					routingKey: "hello",
+					basicProperties: null,
+					body: body);
+			}
+			await Task.CompletedTask;
+		}
+
+		public async Task ReadMessageFromBroker(CancellationToken cancellationToken)
 		{
 			var consumer = new EventingBasicConsumer(_channel);
 			consumer.Received += (model, ea) =>
@@ -25,12 +51,21 @@ namespace Challenge.Infrastructure.Adapters
 				var body = ea.Body.ToArray();
 				var message = Encoding.UTF8.GetString(body);
 				// Procesa el mensaje aquí
-				Console.WriteLine($"[x] Received {message}");
 			};
 
-			_channel.BasicConsume(queue: "tu_cola", autoAck: true, consumer: consumer);
+			_channel.BasicConsume(queue: nameof(Reservation), autoAck: true, consumer: consumer);
 
 			await Task.CompletedTask;
+		}
+
+		private ConnectionFactory CreateConnectionFactory()
+		{
+			return new ConnectionFactory()
+			{
+				HostName = _rabbitMQOptions.Value.HostName,
+				UserName = _rabbitMQOptions.Value.UserName,
+				Password = _rabbitMQOptions.Value.Password
+			};
 		}
 
 		public void Dispose()
